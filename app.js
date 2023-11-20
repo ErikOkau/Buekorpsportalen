519a0bb94db505239b92a39a88bbed74d50950fe
ErikOkau <erikmsorheim@yahoo.no>
2023-11-20 11:06:37 +0100

    ae

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/register.html'));
+})
 
 app.get('/showDB', (req, res) => {
   const stmt = db.prepare('SELECT * FROM users')
